a59732fc35e51ec1b5a71565e0a37dcc4a5262ec
SicusD <109090248+SicusD@users.noreply.github.com>
2022-08-04 11:51:17 -0500

    Delete MavenProject directory

--- MavenProject/src/main/java/ourTest/MavenProject/App.java
+++
@@ -1,8 +0,0 @@
-package ourTest.MavenProject;
-
-public class App {
-
-  public static void main(String[] args) {
-    System.out.println("Hello from the main method");
-  }
-}
--- MavenProject/src/test/java/ourTest/MavenProject/AppTest.java
+++
@@ -1,9 +0,0 @@
-package ourTest.MavenProject;
-
-import ourTest.MavenProject.App;
-
-public class AppTest {
-  String sha1;
-  private App app;
-
-}
