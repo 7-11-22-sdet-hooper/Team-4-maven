1036bbc3ed0d29bc1ce019d338756b010c0d3a0f
SicusD <109090248+SicusD@users.noreply.github.com>
2022-08-04 12:16:12 -0500

    Add files via upload

---
+++ src/main/java/ourTest/MavenProject/MavenCalculator.java
@@ -0,0 +1,10 @@
+package ourTest.MavenProject;
+
+public class MavenCalculator {
+	public static int multiply(int x, int y) {
+		return (x*y);
+	}
+	public static int add(int x, int y) {
+		return (x+y);
+	}
+}
